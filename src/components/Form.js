8622328a35301d744528c6591929b11bf7232c66
James Wu <ichangjwu@outlook.com>
2022-07-30 02:30:56 +0900

    Solve this binding issue

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,16 +3,23 @@ import General from "./General";
 import Education from "./Education";
 
 class Form extends Component {
+  constructor() {
+    super()
+
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
   
-  // onSubmitForm(e) {
-  //   e.preventDefault();
-  //   console.log('called');
-  //   this.props.changePage()
-  // }
+  onSubmitForm(e) {
+    e.preventDefault();
+    console.log(this);
+    this.props.changePage()
+  }
   
   render() {
+    
+
     return (
-      <form onSubmit={this.props.changePage}>
+      <form onSubmit={this.onSubmitForm}>
         <General />
         <hr />
         <Education />
